e64295c7983cac09962e1130c851fbc34b5c61c5
Yusuf <yusufjaelani33@gmail.com>
2025-12-24 09:52:45 +0700

    fix: add vercel blob domain for images

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,10 @@ const nextConfig: NextConfig = {
         hostname: 'vswumimagvxnzmiszezm.supabase.co',
         pathname: '/storage/v1/object/public/**',
       },
+      {
+        protocol: 'https',
+        hostname: '*.public.blob.vercel-storage.com',
+      },
     ],
   },
 };
